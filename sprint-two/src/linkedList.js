31b2a0e99cb46d7f00db35531b54e2ec49595ce1
Cody Scholberg <cscholberg@gmail.com>
2015-05-15 13:37:34 -0400

    finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+ // reference Node and pass in value to latest Node/next\
+    var node = Node(value);
+    if (list.tail){
+      list.tail.next = node;
+    } 
+    list.tail = node;
+    if (list.head === null){
+      list.head = list.tail;}
   };
 
   list.removeHead = function(){
+    //temp var for current head
+    var temp = list.head.value;
+    // delete current head
+    list.head = list.head.next;
+    // move head to new first node
+    //return temp var
+    return temp;
   };
 
   list.contains = function(target){
+    // target === currentNode.value return true
+    var contains = false;
+    function recursor(node){
+      if(node.value === target){
+        contains = true;
+      } else {
+        if(node.next)
+          recursor(node.next);
+      }
+      //recursive call to list.contains unless list.head.next threequals list.tail
+    }
+    recursor(list.head);
+    return contains;
   };
 
   return list;
@@ -27,3 +55,5 @@ var Node = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ //O(1) for insertion/removal

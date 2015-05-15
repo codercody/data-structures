082725df78e7237b99d485508a882392bf032290
Cody Scholberg <cscholberg@gmail.com>
2015-05-15 18:32:20 -0400

    finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,32 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
-};
+  this._storage[item] = item;
+}
+;
 
 setPrototype.contains = function(item){
+  for(var key in this._storage){
+    if(key === item)
+      return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ //Add O(1)
+ //Contains O(n)
+ //Remove O(1)
